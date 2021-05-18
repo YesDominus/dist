ab74d23c83e5a82c2503b69b304ed6e3fb6ceb4e
YesDominus <83821112+YesDominus@users.noreply.github.com>
2021-05-18 11:09:50 +0200

    Update global.js

--- global.js
+++ global.js
@@ -1,1 +1,13 @@
-console.log("");
+function showDiv(){
+        let ReadMoreDiv = document.getElementById("ReadMoreDiv");
+        let readMoreLabel = document.getElementById("readMoreLabel");
+        
+        if(ReadMoreDiv.style.display === "none"){
+            ReadMoreDiv.style.display = "block"
+            readMoreLabel.innerHTML = "Read less ^" 
+        }else{
+            ReadMoreDiv.style.display = "none";
+            readMoreLabel.innerHTML = "Read more ⌄"
+        }
+    }
+
