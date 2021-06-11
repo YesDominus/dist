6995504e0dc48b1a433e33e01b37f8805f379523
YesDominus <83821112+YesDominus@users.noreply.github.com>
2021-06-11 14:20:03 +0200

    Update global.js

--- global.js
+++ global.js
@@ -10,19 +10,6 @@
         }
 };
 
-function showDiv(){
-        let ReadMoreDiv = document.getElementById("ReadMoreDiv");
-        let readMoreLabel = document.getElementById("readMoreLabel");
-        
-        if(ReadMoreDiv.style.display === "none"){
-            ReadMoreDiv.style.display = "block"
-            readMoreLabel.innerHTML = "Read less ^" 
-        }else{
-            ReadMoreDiv.style.display = "none";
-            readMoreLabel.innerHTML = "Read more ⌄"
-        }
-    }
-
 /* NEW PAGE NAVIGATION
 
 var internalNavID = "navlink";
