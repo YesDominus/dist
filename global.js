8f478966f4b28fbf5288779424e7b49e0ae2d722
YesDominus <83821112+YesDominus@users.noreply.github.com>
2021-09-01 10:35:30 +0200

    Update global.js

--- global.js
+++ global.js
@@ -103,3 +103,265 @@ if(navBarUL !== null){
  }
 };
 }
+
+/* Handling the page post part */
+let quickAddButton = document.querySelector("#quick-add-button");
+
+console.log("URI", superNova.get_dir_uri);
+console.log("NONCE", superNova.nonce);
+
+if(quickAddButton){
+    
+    quickAddButton.addEventListener("click", function() {
+
+    let titleVal = document.querySelector(".admin-quick-add [name='title']").value;
+    let slugVal = document.querySelector(".admin-quick-add [name='slug']").value;
+    let parentVal = document.querySelector(".admin-quick-add [name='parent']").value;
+    
+    let firstParagraphVal =  document.querySelector(".admin-quick-add [name='firstParagraphContent']").value;
+    let readMorePassageVal =  document.querySelector(".admin-quick-add [name='readMorePassageContent']").value;
+    let firstPassageVal =  document.querySelector(".admin-quick-add [name='firstPassageContent']").value;
+    let secondPassageVal =  document.querySelector(".admin-quick-add [name='secondPassageContent']").value;
+    let thirdPassageVal = document.querySelector(".admin-quick-add [name='thirdPassageContent']").value;
+    let fourthPassageVal = document.querySelector(".admin-quick-add [name='fourthPassageContent']").value;
+    let fifthPassageVal = document.querySelector(".admin-quick-add [name='fifthPassageContent']").value;
+    let sixthPassageVal = document.querySelector(".admin-quick-add [name='sixthPassageContent']").value;
+    let seventhPassageVal = document.querySelector(".admin-quick-add [name='seventhPassageContent']").value;
+    let eigthPassageVal = document.querySelector(".admin-quick-add [name='eigthPassageContent']").value;
+    let ninthPassageVal = document.querySelector(".admin-quick-add [name='ninthPassageContent']").value;
+    let faqVal = document.querySelector(".admin-quick-add [name='faqContent']").value;
+    
+    if (titleVal === "" || slugVal === "" || parentVal === "") {
+     
+    alert("Title, Slug and Parent ID must be filled out");
+    return false;
+    }
+    
+    let passageContents = [readMorePassageVal, firstPassageVal, secondPassageVal, thirdPassageVal, fourthPassageVal, fifthPassageVal, sixthPassageVal, seventhPassageVal, eigthPassageVal, ninthPassageVal];
+    
+    let allSectionsContent = "";
+    
+    if(firstParagraphVal !== ""){
+        allSectionsContent = firstParagraphVal;
+    }
+    
+    //console.log(passageContents);
+    
+    for (let i = 0; i < passageContents.length; i++) {
+        let currentPassageText = passageContents[i];
+        
+        if(currentPassageText !== ""){
+            
+        let sections = [];
+        
+    var section = document.createElement("SECTION");
+    //passage.setAttribute("class", "passage");
+    
+    var parser = new DOMParser();
+    
+
+    var doc = textValue(currentPassageText);
+    
+    let passageContainer = document.createElement("DIV");
+    passageContainer.setAttribute("class", "passage");
+
+    let passageHeader = document.createElement("DIV");
+    passageHeader.setAttribute("class", "passage-header");
+    
+    let headerH2 = document.createElement("H2");
+    
+    let firstH2 = doc.querySelectorAll("H2")[0];
+    
+    headerH2.appendChild(document.createTextNode(firstH2.innerText));
+    
+    passageContainer.appendChild(passageHeader);
+    
+    let passageBody = document.createElement("DIV");
+    passageBody.setAttribute("class", "passage-body");
+    passageContainer.appendChild(passageBody);
+    
+    
+    section.appendChild(passageContainer);
+    
+    passageBody.append(...doc.body.children);
+    
+    let removedH2 = section.querySelectorAll("H2")[0].remove();
+    passageHeader.appendChild(headerH2);
+    
+    if(i > 0){
+    
+    let headerIMG = document.createElement("IMG");
+    headerIMG.setAttribute("class", "passage-img");
+    headerIMG.setAttribute("src", "xxxxxxxxxxxxxxxxxxx");
+    
+    let passageHeaderIMG = document.createElement("DIV");
+    passageHeaderIMG.appendChild(headerIMG);
+    
+    passageHeader.appendChild(passageHeaderIMG);
+    }
+    
+    //passageHeader.appendChild(document.createTextNode(section.querySelectorAll("H2")[0].innerHTML));
+    
+    
+    //console.log(section);
+   
+    let allChildren = section.getElementsByTagName('*');
+    
+    //let addingNewPassage = [];
+    
+    for (let i = 0; i < allChildren.length; i++) {
+        let currentElement = allChildren[i];
+        
+        if(currentElement.tagName === "P"){
+        
+        if(currentElement.childNodes.length < 1){
+            currentElement.remove();
+        } else{
+            currentElement.innerHTML = currentElement.innerHTML.trim();
+        }
+        }
+        
+    }
+    
+    if(i === 0){
+        
+        //console.log(" is raedmore");
+        let tempDiv = document.createElement("DIV");
+        
+        let navBarContainer = document.createElement("DIV");
+        navBarContainer.setAttribute("class", "navBarContainer");
+        navBarContainer.innerHTML = '[sc name="internal-navbar"]';
+        
+        let readMoreParagraph = document.createElement("P");
+
+        let readMoreLabel = document.createElement("LABEL");
+        readMoreLabel.setAttribute("id", "readMoreLabel");
+        readMoreLabel.innerHTML = "Read more ⌄";
+        readMoreParagraph.appendChild(readMoreLabel);
+        
+        let readMoreDiv = document.createElement("DIV");
+        readMoreDiv.setAttribute("id", "ReadMoreDiv");
+        readMoreDiv.appendChild(section);
+        
+        section.insertBefore(navBarContainer, section.firstChild);
+        tempDiv.append(readMoreParagraph);
+        tempDiv.append(readMoreDiv);
+        
+        sections.push(tempDiv.innerHTML);
+        
+    }
+    else{
+        sections.push(section.outerHTML);
+    }
+    for (let i = 0; i < sections.length; i++) {
+        allSectionsContent += sections[i];
+    }    
+    
+    
+        } 
+    }
+    
+    //console.log("SECTIONS: ", sections);
+
+
+        function textValue(text){
+        return parser.parseFromString(text, 'text/html');
+    }
+    
+    if(faqVal){
+        var passage = document.createElement("DIV");
+    passage.setAttribute("class", "passage");
+    
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(faqVal, 'text/html');
+    
+    passage.append(...doc.body.children);
+    
+    
+    var faqSection = document.createElement("SECTION");
+    var faqContainerHeader = document.createElement("H2");
+    faqContainerHeader.setAttribute("class", "faq-h");
+    faqContainerHeader.appendChild(document.createTextNode(passage.querySelectorAll("H2")[0].innerText)); 
+
+    var faqContainer = document.createElement("DIV");
+    faqContainer.setAttribute("class", "schema-faq-code");
+    
+
+    var faqParagraph = document.createElement("P");
+    
+    let h3List = passage.querySelectorAll("H3");
+    let paragraphList = passage.querySelectorAll("P");
+    
+    for (let i = 0; i < h3List.length; i++) {
+            var faqQuestionContainer = document.createElement("DIV");
+            
+            faqQuestionContainer.setAttribute("class", "faq-question");
+        
+            let currentH3 = h3List[i];
+            let faqHeader = document.createElement("H3");
+            faqHeader.setAttribute("class", "faq-q");
+            faqHeader.appendChild(document.createTextNode(currentH3.innerText));
+            faqQuestionContainer.appendChild(faqHeader);
+            
+            var faqParagraphContainer = document.createElement("DIV");
+            
+            let currentParagraph = paragraphList[i];
+            let faqParagraph = document.createElement("P");
+            faqParagraph.setAttribute("class", "faq-a");
+            faqParagraph.appendChild(document.createTextNode(currentParagraph.innerText));
+            faqParagraphContainer.appendChild(faqParagraph);
+            
+            faqQuestionContainer.appendChild(faqParagraphContainer);
+            faqContainer.appendChild(faqQuestionContainer);
+            
+    }
+    
+    faqSection.appendChild(faqContainerHeader);
+    
+    faqSection.appendChild(faqContainer);
+    
+    allSectionsContent += faqSection.outerHTML;
+
+
+ //   console.log("CHECKA DEN HÄR:", faqSection);
+    
+    }
+
+/*
+<section>
+<h2 class="faq-h">FAQ's About Fast Payouts at Arizona Gambling Sites</h2>
+<div class="schema-faq-code">
+
+<div class="faq-question">
+<h3 class="faq-q">XXXXXXXXXXXXXXXXXXXXXXXXX</h3>
+<div>
+<p class="faq-a">xxxxxxxxxxxxxxxx</p>
+</div>
+</div>
+
+</div>
+</section>
+*/
+
+
+if(allSectionsContent !== ""){
+
+var createPost = new XMLHttpRequest();
+var ourPostData = {
+"title": titleVal,
+"content": allSectionsContent,
+"status": "publish",
+//"metaDesc": "mfmdk",
+"slug" : slugVal,
+"parent": parentVal
+}
+
+createPost.open("POST", superNova.siteURL + "/wp-json/wp/v2/pages");
+createPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+createPost.setRequestHeader("X-WP-Nonce", superNova.nonce);
+createPost.send(JSON.stringify(ourPostData));
+alert("The page is now posted");
+}
+
+    });
+}
