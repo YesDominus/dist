bbe1fc1ad4a05a4f0493a63a364d9c3acd80d78d
YesDominus <83821112+YesDominus@users.noreply.github.com>
2021-09-02 10:59:18 +0200

    Update post-content.js

--- post-content.js
+++ post-content.js
@@ -2,10 +2,12 @@
  
 let quickAddButton = document.querySelector("#quick-add-button");
 
-console.log("URI", superNova.get_dir_uri);
-console.log("NONCE", superNova.nonce);
-
 if(quickAddButton){
+ 
+    /* Creating HTML form */
+var html = '<div class="admin-quick-add"><h3>Quick Add Page</h3><input name="title" type="text" placeholder="Title/H1"><input name="slug" type="text" placeholder="SLUG. ex: /fast-payouts/"><input name="parent" type="number" placeholder="Parent ID. ex: 2123 --- LEAVE EMPTY IF PATRIARKATET"><!--<input disabled="disabled" name="metaTitle" type="text" placeholder="Meta Title"><input disabled="disabled" name="metaDescription" type="text" placeholder="Meta description if exists">--><textarea name="firstParagraphContent" placeholder="First paragraph"></textarea><textarea name="readMorePassageContent" placeholder="Read more passage"></textarea><textarea name="firstPassageContent" placeholder="First passage"></textarea><textarea name="secondPassageContent" placeholder="Second passage"></textarea><textarea name="thirdPassageContent" placeholder="Third passage"></textarea><textarea name="fourthPassageContent" placeholder="Fourth passage"></textarea><textarea name="fifthPassageContent" placeholder="Fifth passage"></textarea><textarea name="sixthPassageContent" placeholder="Sixth passage"></textarea><textarea name="seventhPassageContent" placeholder="Seventh passage"></textarea><textarea name="eigthPassageContent" placeholder="Eigth passage"></textarea><textarea name="ninthPassageContent" placeholder="Ninth passage"></textarea><textarea name="faqContent" placeholder="Faq Content"></textarea><button id="quick-add-button">Create Post</button></div>';
+document.getElementById("postContainer").innerHTML = html;
+ /* End of HTML form */
     
     quickAddButton.addEventListener("click", function() {
 
