b3c9fdf14082f956c747c757dafd5fa78fff4f12
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-02-12 13:51:26 +0100

    Create create-html-form-and-post-test2.js

---
+++ create-html-form-and-post-test2.js
@@ -0,0 +1,15 @@
+var createPost = new XMLHttpRequest();
+var ourPostData = {
+"title": "Test1",
+"content": "<p>hej</p>",
+"status": "private",
+//"metaDesc": "mfmdk",
+"slug" : "/test/",
+"parent": 3679
+}
+
+createPost.open("POST", superNova.siteURL + "/wp-json/wp/v2/pages");
+createPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+createPost.setRequestHeader("X-WP-Nonce", superNova.nonce);
+createPost.send(JSON.stringify(ourPostData));
+alert("The page is now posted");
