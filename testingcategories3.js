0cb3b62bcf34dc6e81d8342513ddb126285b6101
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-04-16 16:11:12 +0200

    Update testingcategories3.js

--- testingcategories3.js
+++ testingcategories3.js
@@ -379,40 +379,54 @@ alert("The page is now posted");
 }
 */
 let attribute = null;
-let operators = [{
+let operators = [
+	{
         brand: "fanduel",
         bankings: ["paypal"],
         attributes: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "formula-1", "motorsports", "rugby", "snooker", "table-tennis", "horse-racing"]
-    }, {
+    	},
+	
+	{
         brand: "betrivers",
         bankings: ["paypal", "visa", "mastercard", "discovercard", "pay-with-cash", "ach", "vip-preferred", "casino-cage", "online-banking", "trustly"],
         attributes: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "esports", "floorball", "formula-1", "gaelic-sports", "handball", "jai-alai", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics"]
-    }, {
+    	},
+	
+	{
         brand: "sugarhouse",
         bankings: ["paypal", "visa", "mastercard", "discovercard", "pay-with-cash", "ach", "vip-preferred", "casino-cage", "online-banking", "trustly", "paynearme", "wire-transfer"],
         attributes: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "esports", "floorball", "formula-1", "gaelic-sports", "handball", "jai-alai", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics"]
-    }, {
+    	},
+	
+	{
         brand: "caesars",
         bankings: ["paypal", "playplus", "online banking", "visa", "mastercard"],
         attributes: ["australian-rules", "motorsports", "baseball", "basketball", "boxing", "cricket", "cycling", "darts", "football", "golf", "hockey", "lacrosse", "rugby", "snooker", "soccer", "softball", "table-tennis", "tennis", "mma"]
-    }, {
+    	},
+	
+	{
         brand: "fubo",
         bankings: ["visa", "mastercard", "ach", "vip-preferred", "skrill", "playplus", "paynearme", "paywithmybank", "trustly"],
         attributes: ["football", "motorsports", "baseball", "hockey", "soccer", "mma", "tennis", "basketball", "boxing", "golf", "formula-1"]
-    }, {
+    	},
+	
+	{
         brand: "betmgm",
         bankings: ["visa", "mastercard", "vip-preferred", "apple-pay", "playplus", "online-banking", "check-by-email"],
         attributes: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "badminton", "floorball", "formula-1", "gaelic-sports", "handball", "lacrosse", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics", "water-polo", "ski-jumping"]
-    }, {
+    	},
+	
+	{
         brand: "unibet",
         bankings: ["paypal", "visa", "playplus", "paynearme", "mastercard", "ach", "vip-preferred"],
         attributes: ["football", "basketball", "baseball", "hockey", "tennis", "soccer", "mma", "golf", "esports", "e-sports", "motorsports", "table-tennis", "australian-rules", "boxing", "cricket", "cycling", "darts", "floorball", "boxing", "formula-1", "gaelic-sports", "handball", "rugby", "snooker", "volleyball", "olympics"]
-    }, {
+    	},
+	
+	{
         brand: "betfred",
         bankings: ["paypal"],
         attributes: ["soccer", "esports", "e-sports", "baseball"]
-    }
-
+    	}
 ];
 
 
