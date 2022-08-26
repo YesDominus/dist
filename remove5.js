59035b8be78558cb953228b6b7860d8f78aa4da1
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 16:47:26 +0200

    Delete remove5.js

--- remove5.js
+++
@@ -1,7 +0,0 @@
-var js = document.createElement("script");
-
-js.type = "text/javascript";
-js.src = "https://cdn.jsdelivr.net/gh/YesDominus/dist@main/set-script2.min.js";
-
-document.head.appendChild(js);
-console.log(thescript);
