0a488ac4b8241a33e2a9499c9a2b829c8d5ad5b5
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:45:15 +0200

    Delete global10.js

--- global10.js
+++
@@ -1,4 +0,0 @@
-const scriptTag = document.createElement("script");
-scriptTag.src = "https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js";
-document.body.appendChild(scriptTag);
-    console.log(fanduel);
