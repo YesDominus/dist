cfaa54a564e397455c48b4b3fce486076727f7be
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:28:12 +0200

    Delete global6.js

--- global6.js
+++
@@ -1,4 +0,0 @@
-  $.getScript("https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js", function (data) {
-    console.log(data); // Text of JS file
-    console.log("Load was performed.");
-  });
