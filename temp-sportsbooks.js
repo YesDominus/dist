57e80fd784e038ba33e43ee1962152bd00db46c6
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 19:56:15 +0200

    Create temp-sportsbooks.js

---
+++ temp-sportsbooks.js
@@ -0,0 +1,321 @@
+// FANDUEL
+
+let fanduelStandardSportsBonus = "Bet $5 get $150 - win or lose"
+let fanduelStandardCasinoBonus = "$1,000 play it again";
+let fanduel = {
+    brand: "Fanduel",
+    promoCode: "Click & activate",
+    azBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    coBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    ctBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    iaBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    inBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    laBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    miBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    nyBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    nvBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    ksBonus: {
+        sportsbook: "Early sign-up $100 free bet",
+        casino: fanduelStandardCasinoBonus
+    },
+    njBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    paBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    tnBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    vaBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    wvBonus: {
+        sportsbook: fanduelStandardSportsBonus,
+        casino: fanduelStandardCasinoBonus
+    },
+    sports: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "formula-1", "motorsports", "rugby", "snooker", "table-tennis", "horse-racing"],
+    bankings: ["paypal"]
+};
+
+// BETMGM
+	let betmgmStandardSportsBonus = "Risk-free bet up to $1000"
+  let betmgmStandardCasinoBonus = "$25 free play";
+	let betmgm = {
+    brand: "BetMGM",
+    promoCode: "Click & activate",
+    azBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    coBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    ctBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    iaBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    inBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    laBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    miBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    nyBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    nvBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    ksBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    njBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    paBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    tnBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    vaBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+    wvBonus: {
+        sportsbook: betmgmStandardSportsBonus,
+        casino: betmgmStandardCasinoBonus
+    },
+        bankings: ["visa", "mastercard", "vip-preferred", "apple-pay", "playplus", "online-banking", "check-by-email"],
+        sports: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "badminton", "floorball", "formula-1", "gaelic-sports", "handball", "lacrosse", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics", "water-polo", "ski-jumping"]
+    	};
+
+// CAESARS
+	let caesarsStandardSportsBonus = "Up to $1,500 risk-free"
+  let caesarsStandardCasinoBonus = "$2,000 bonus";
+	let caesars = {
+        brand: "caesars",
+        promoCode: "Click & activate",
+    azBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    coBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    ctBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    iaBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    inBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    laBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    miBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    nyBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    nvBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    ksBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    njBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    paBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    tnBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    vaBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+    wvBonus: {
+        sportsbook: caesarsStandardSportsBonus,
+        casino: caesarsStandardCasinoBonus
+    },
+        bankings: ["paypal", "playplus", "online banking", "visa", "mastercard"],
+        sports: ["australian-rules", "motorsports", "baseball", "basketball", "boxing", "cricket", "cycling", "darts", "football", "golf", "hockey", "lacrosse", "rugby", "snooker", "soccer", "softball", "table-tennis", "tennis", "mma"]
+    	};
+
+// WYNNBET
+
+let wynnBETStandardSportsBonus = "Bet $100 get $100"
+let wynnBETStandardCasinoBonus = "100% deposit match up to $1000";
+let wynnbet = {
+        brand: "WynnBET",
+        promoCode: "250MATCH",
+    azBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    coBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    ctBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    iaBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    inBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    laBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    miBonus: {
+        sportsbook: "Bet $100 get $50",
+        casino: wynnBETStandardCasinoBonus
+    },
+    nyBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    nvBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    ksBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    njBonus: {
+        sportsbook: "Bet $100 get $50",
+        casino: wynnBETStandardCasinoBonus
+    },
+    paBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    tnBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    vaBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+    wvBonus: {
+        sportsbook: wynnBETStandardSportsBonus,
+        casino: wynnBETStandardCasinoBonus
+    },
+        bankings: ["paypal", "visa", "playplus", "paynearme", "mastercard", "ach", "vip-preferred"],
+        sports: ["football", "basketball", "baseball", "hockey", "tennis", "soccer", "mma", "golf", "esports", "e-sports", "motorsports", "table-tennis", "australian-rules", "boxing", "cricket", "cycling", "darts", "floorball", "boxing", "formula-1", "gaelic-sports", "handball", "rugby", "snooker", "volleyball", "olympics"]
+    	};
+
+//BETRIVERS
+
+	let betrivers = {
+        brand: "betrivers",
+        bankings: ["paypal", "visa", "mastercard", "discovercard", "pay-with-cash", "ach", "vip-preferred", "casino-cage", "online-banking", "trustly"],
+        sports: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "esports", "e-sports", "floorball", "formula-1", "gaelic-sports", "handball", "jai-alai", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics"]
+    	};
+
+// FUBO
+	
+	let fubo = {
+        brand: "fubo",
+        bankings: ["visa", "mastercard", "ach", "vip-preferred", "skrill", "playplus", "paynearme", "paywithmybank", "trustly"],
+        sports: ["football", "motorsports", "baseball", "hockey", "soccer", "mma", "tennis", "basketball", "boxing", "golf", "formula-1"]
+    	};
+
+// UNIBET
+	
+	let unibet = {
+        brand: "unibet",
+        bankings: ["paypal", "visa", "playplus", "paynearme", "mastercard", "ach", "vip-preferred"],
+        sports: ["football", "basketball", "baseball", "hockey", "tennis", "soccer", "mma", "golf", "esports", "e-sports", "motorsports", "table-tennis", "australian-rules", "boxing", "cricket", "cycling", "darts", "floorball", "boxing", "formula-1", "gaelic-sports", "handball", "rugby", "snooker", "volleyball", "olympics"]
+    	};
+
+//BETFRED
+	
+	let betfred = {
+        brand: "betfred",
+        bankings: ["paypal"],
+        sports: ["soccer", "esports", "e-sports", "baseball"]
+    	};
+
+//SUGARHOUSE
+	
+	let sugarhouse = {
+        brand: "sugarhouse",
+        bankings: ["paypal", "visa", "mastercard", "discovercard", "pay-with-cash", "ach", "vip-preferred", "casino-cage", "online-banking", "trustly", "paynearme", "wire-transfer"],
+        sports: ["football", "basketball", "hockey", "baseball", "soccer", "tennis", "golf", "mma", "australian-rules", "boxing", "cricket", "cycling", "darts", "esports", "floorball", "formula-1", "gaelic-sports", "handball", "jai-alai", "motorsports", "rugby", "snooker", "table-tennis", "volleyball", "olympics"]
+    	};
