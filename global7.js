0bfe6b0275f5ccd144a1ec4b4396d54a6542f555
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:33:13 +0200

    Delete global7.js

--- global7.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const utilsFile = await fetch("https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js");
-  const utilsText = await utilsFile.text();
-  eval(utilsText);
-  console.log(fanduel);
-});
