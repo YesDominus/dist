3dc7f0d942325549d020cf0606b434608dd693ff
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:43:00 +0200

    Delete global9.js

--- global9.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const utilsFile = await fetch("https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js");
-  const utilsText = await utilsFile.fanduel;
-  eval(utilsText);
-});
