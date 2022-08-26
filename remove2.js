b94ed3cfb186648d304be02bce0c81a624cf4b29
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 16:07:21 +0200

    Create remove2.js

---
+++ remove2.js
@@ -0,0 +1,8 @@
+var js = document.createElement("script");
+
+js.type = "text/javascript";
+js.src = https://cdn.jsdelivr.net/gh/YesDominus/dist@main/set-script2.min.js;
+
+document.body.appendChild(js);
+console.log(thescript);
+</script>
