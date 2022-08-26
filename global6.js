1c5f1ebd953107683b92f8bf6fe5e97440915467
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:22:36 +0200

    Create global6.js

---
+++ global6.js
@@ -0,0 +1,4 @@
+  $.getScript("https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js", function (data) {
+    console.log(data); // Text of JS file
+    console.log("Load was performed.");
+  });
