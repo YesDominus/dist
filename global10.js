7fd9f64a465999e0fdb6d38c971b5a48db76ff2e
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 18:44:06 +0200

    Create global10.js

---
+++ global10.js
@@ -0,0 +1,4 @@
+const scriptTag = document.createElement("script");
+scriptTag.src = "https://cdn.jsdelivr.net/gh/YesDominus/dist@main/brand-fanduel.min.js";
+document.body.appendChild(scriptTag);
+    console.log(fanduel);
