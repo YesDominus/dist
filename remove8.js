a9ada44336855d95b21e07576edeb77d6ab5e513
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-08-26 16:47:37 +0200

    Delete remove8.js

--- remove8.js
+++
@@ -1,9 +0,0 @@
-// import the variables and function from module.js
-import { message, number, multiplyNumbers } from 'https://cdn.jsdelivr.net/gh/YesDominus/dist@main/remove7.min.js';
-
-console.log(message); // hello world
-
-console.log(number); // 10
-
-console.log(multiplyNumbers(3, 4)); // 12
-console.log(multiplyNumbers(5, 8)); // 40
