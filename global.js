83a50e55224125c53c73ec49efd00192f61d1a21
YesDominus <83821112+YesDominus@users.noreply.github.com>
2022-09-23 08:08:05 +0200

    Update global.js

--- global.js
+++ global.js
@@ -1098,6 +1098,37 @@ floatingGoToTopBtn.onclick = function() {
 	document.documentElement.scrollTop = 0;
 };
 
+/* Adding the floating review CTA button */
+let table = document.getElementsByClassName("top-list-table")[0];
+if (table) {
+	let floatingAnchor = document.createElement("a");
+	floatingAnchor.setAttribute("id", "floating-review-cta-button");
+	floatingAnchor.setAttribute("href", "");
+	floatingAnchor.innerHTML = '<p id="textId"></p><ul id="promoList"></ul>';
+	document.getElementsByTagName("FOOTER")[0].appendChild(floatingAnchor);
+
+	let currentIMG = table.getElementsByClassName("imgTD")[0].getElementsByTagName("img")[0];
+	document.getElementById("floating-review-cta-button").style.backgroundColor = table.getElementsByClassName("imgTD")[0].style.backgroundColor;
+
+	let bonusText = "<span class='current-bonus-text'>" + table.getElementsByClassName("bonusTD")[0].innerHTML + '</span>';
+	document.getElementById("textId").innerHTML = "🥇<img class='current-brand-img' src=" + currentIMG.src + "> " + bonusText;
+
+	document.getElementById("promoList").innerHTML = table.getElementsByClassName("promo-code-offer")[0].outerHTML;
+	let currentHref = table.getElementsByClassName("ctaAnchor")[0].href;
+
+	let wordsInHref = currentHref.split("_");
+	const lastWord = wordsInHref[wordsInHref.length - 1];
+
+	if (lastWord === "button") {
+		document.getElementById("floating-review-cta-button").href = currentHref + "_floating_cta";
+	} else {
+		document.getElementById("floating-review-cta-button").href = currentHref;
+	}
+
+} else {
+	console.log("table not found");
+}
+
 /* Handling the page post part */
 
 /* 
